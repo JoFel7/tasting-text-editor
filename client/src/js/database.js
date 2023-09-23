c37075cfa10085d8d64dc33205a6a930d501f058
JoFel7 <josuefeliciano7@gmail.com>
2023-09-22 22:54:59 -0400

    js work in database and install

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,51 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+const DB_NAME = 'jate';
+const DB_VERSION = 1;
+const OBJECT_STORE_NAME = 'jate';
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const initDb = async () => {
+  try {
+    const db = await openDB(DB_NAME, DB_VERSION, {
+      upgrade(db) {
+        if (!db.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+          const store = db.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id', autoIncrement: true });
+          console.log('jate database created', store);
+        } else {
+          console.log('jate database already exists');
+        }
+      },
+    });
+  } catch (error) {
+    console.error('Error initializing the database:', error);
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB(DB_NAME, DB_VERSION);
+    const tx = db.transaction(OBJECT_STORE_NAME, 'readwrite');
+    const store = tx.objectStore(OBJECT_STORE_NAME);
+    const req = store.add({ value: content });
+    const result = await req;
+    console.log('Content added to the database:', result.value);
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  }
+};
 
-initdb();
+export const getDb = async () => {
+  try {
+    const db = await openDB(DB_NAME, DB_VERSION);
+    const tx = db.transaction(OBJECT_STORE_NAME, 'readonly');
+    const store = tx.objectStore(OBJECT_STORE_NAME);
+    const content = await store.get(1);
+    return content;
+  } catch (error) {
+    console.error('Error getting content from the database:', error);
+    return null;
+  }
+};
+
+// Initialize the database
+initDb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,30 @@
-const butInstall = document.getElementById('buttonInstall');
+const installButton = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+const handleBeforeInstallPrompt = (event) => {
+  event.preventDefault(); // Prevent the default installation prompt
+  installButton.style.display = "block"; // Show your custom install button
+};
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implement a click event handler on the `butInstall` element
+installButton.addEventListener("click", async () => {
+  installButton.style.display = "none"; // Hide the custom install button
+  const promptEvent = window.beforeInstallPromptEvent;
+  if (promptEvent) {
+    promptEvent.prompt(); // Show the native installation prompt
+    const choiceResult = await promptEvent.userChoice;
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the installation");
+    } else {
+      console.log("User dismissed the installation");
+    }
+  }
+});
+
+// Add a handler for the `appinstalled` event
+window.addEventListener("appinstalled", () => {
+  console.log("App was successfully installed");
+});
